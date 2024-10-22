0559edfd9cecdcb6dbee08e3e3f21f778c3f34c3
Vineet Mishra <02vineetmishra@gmail.com>
2024-10-22 15:27:24 +0530

    cd

--- src/utils/list.js
+++ src/utils/list.js
@@ -1,7 +1,7 @@
 const navigation = [
   { name: 'Home', href: '/' },
   { name: 'Rajasthan Tour Pakages', href: 'rajasthan-tour' },
-  { name: 'India Tour Pakages', href: 'about' },
+  // { name: 'India Tour Pakages', href: 'about' },
   { name: 'Services', href: 'services' },
   { name: 'Contact', href: '/contact-us' },
   { name: 'FeedBack', href: '/rating' },
